80c6bec6f5c83730adaf234bb19ce296b56b4aa2
Adrian McEwen <adrianm@mcqn.com>
2019-03-26 22:51:06 +0000

    Add code to monitor pedalling speed and play encouragement.  Developed for the Gadget Show's visit

---
+++ cadence-media-player/mqtt-controller.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/ruby
+require 'mqtt'
+require 'json'
+require 'base64'
+require 'yaml'
+require 'net/https'
+
+# Read in config
+settings = nil
+if ARGV.length == 1
+  settings = YAML.load_file(ARGV[0])
+else
+  puts "No configuration provided."
+  exit
+end
+
+# Keep track of the extremities of values reported
+highest = [0,0]
+lowest = [123456,123456]
+
+# TODO Read this in from the config file
+thresholds = [10, 10]
+
+# Digital version of readings, and last version too
+digital = []
+last_digital = [false, false]
+step_count = 5
+last_steps = []
+step_count.times { |i| last_steps.unshift(i*2000) }
+
+desired_cadence = 100
+encouragement_cadence = 80
+reached_cadence = false
+
+MQTT::Client.connect(settings['mqtt']['url']) do |c|
+  c.get(settings['mqtt']['topic']) do |topic, message|
+    #puts "#{topic}: #{message}"
+    readings = message.split(',').map { |x| x.to_i }
+    if readings[0] > highest[0]
+      highest[0] = readings[0]
+    end
+    if readings[0] < lowest[0]
+      lowest[0] = readings[0]
+    end
+    if readings[1] > highest[1]
+      highest[1] = readings[1]
+    end
+    if readings[1] < lowest[1]
+      lowest[1] = readings[1]
+    end
+    digital[0] = readings[0] > thresholds[0]
+    digital[1] = readings[1] > thresholds[1]
+    rising = " "#false
+    if digital[0] and !last_digital[0]
+      rising = "***" #true
+      # Record when this step happened
+      last_steps.pop # get rid of oldest timestamp
+      last_steps.unshift(Time.now.to_f) # store latest
+    end
+    # Work out cadence from the time the last (step_count-1) steps took
+    cadence = (step_count-1)*60/(last_steps.first - last_steps.last)
+    # Look for reaching cycling speed
+    if cadence > desired_cadence and !reached_cadence
+      reached_cadence = true
+      puts "####################### WELL DONE!"
+      # Congratulate the rider
+      Process.spawn("aplay ./audio/congratulate/AbsolutelyKillingIt.wav")
+    end
+    # See if they've slowed down too much
+    if cadence < encouragement_cadence and reached_cadence
+      reached_cadence = false
+      # Encourage them to cycle faster
+      Process.spawn("aplay ./audio/encourage/YouCanDoIt.wav")
+    end
+      
+    last_digital[0] = digital[0]
+    last_digital[1] = digital[1]
+    puts "#{rising}\t#{cadence}\t#{readings[0]}\t#{digital[0]}\t#{readings[1]}\t#{digital[1]}\t\t0: #{lowest[0]}-#{highest[0]},\t1: #{lowest[1]}-#{highest[1]}"
+  end
+end
+
--- data/README.md
+++ data/README.md
@@ -7,6 +7,11 @@ Data captured with:
   mosquitto_sub -h broker.mqtt-dashboard.com -t "mcqn/#" | tee NAME-OF-LOG-FILE.log
 ```
 
+If you want a quick-and-dirty way to replay any captured data, try:
+```
+  cat NAME-OF-LOG-FILE.log | mosquitto_pub -h broker.mqtt-dashboard.com -t "mcqn/test"
+```
+
 ## Recordings
 
  * [2019-02-09-tapdancing.log](2019-02-09-tapdancing.log) - initial tests with [Sole Rebel Tap](http://solerebeltap.co.uk/).  Lines with a single number are from the left-toe sensor, lines with two numbers are `right-heel:right-toe` values
